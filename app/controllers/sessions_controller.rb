751917a0af399b929f15b6b0659f315f50814acb
streeet25 <streeet25@gmail.com>
2015-09-23 18:36:30 +0300

    overwrite

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      sign_in user
+      redirect_to user, :notice => "Logged in!"
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url, :notice => "Logged out!"
+  end
+end
